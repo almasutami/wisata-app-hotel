fe8d0f080fc8f0dfe057726a2c53609791652d17
almasutami <almasutami@gmail.com>
2024-01-15 13:08:14 +0700

    feat: add icons

--- src/components/app-toolbar.vue
+++ src/components/app-toolbar.vue
@@ -1,11 +1,18 @@
-<script setup></script>
+<script setup>
+import hotelDetails from "../source/hotel-data.json";
+const hotelData = hotelDetails;
+</script>
 
 <template>
   <div class="toolbar">
     <div class="toolbar-container">
       <img alt="Vue logo" src="../assets/logo.png" class="toolbar-logo" />
       <div class="searchbar-container">
-        <input type="text" name="" class="search-input" id="" />
+        <div class="search-input">
+          <font-awesome-icon icon="fa-solid fa-search" class="search-icon" />
+          <div>{{ hotelData?.name }}</div>
+          <div>{{ hotelData?.name }}</div>
+        </div>
       </div>
       <button class="button">Sign in</button>
     </div>
@@ -17,6 +24,7 @@
   padding: 4px 0px;
   height: 64px;
 }
+
 .toolbar-container {
   padding: 8px 16px;
   display: flex;
@@ -33,8 +41,14 @@
   width: 590px;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  align-items: center;
 }
+
+.search-icon {
+  margin-right: 8px;
+  color: #888;
+}
+
 .search-input {
   margin-left: 24px;
   margin-right: 24px;
@@ -46,8 +60,12 @@
   box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.2), 0 0 0 0 rgba(0, 0, 0, 0.14),
     0 0 0 0 rgba(0, 0, 0, 0.12) !important;
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   border: none;
   border-radius: 4px;
+  outline: none;
 }
 
 .search-input:hover {
